cfb8a49550c92e25d4f504ad479f61e4dffd80f6
Jacob Pfeifer <jacob@pfeifer.dev>
2023-05-07 01:01:18 -0400

    register module

--- build.zig
+++ build.zig
@@ -15,6 +15,10 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("validate", .{
+        .source_file = .{ .path = "src/validate.zig" },
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "zig-validate",
         // In this case the main source file is merely a path, however, in more
